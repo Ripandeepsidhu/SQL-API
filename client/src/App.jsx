7056c02b195a5a8b72b9f46c9049a0f19cc90120
Ripandeepsidhu <kaursidhuripandeep@gmail.com>
2025-11-04 10:06:05 +0100

    react ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
 import styles from './index.module.css'
 import sqlLogo from './assets/sql-logo.png'
+import { useState } from 'react'
+
 function App() {
- 
+ const[queryDescription, setQueryDescription] = useState("")
+const onSubmit =(e) => {
+  e.preventDefault();
+  console.log("form submited:", queryDescription)
+}
+
   return (
     <main className={styles.main}>
       <img src={sqlLogo} alt="" className={styles.icon}/>
       <h3>Generate SQL with AI</h3>
 
-      <form>
+      <form onSubmit={onSubmit}>
         <input 
           type="text"
           name="query-description" 
-          placeholder="Describe your query"/>
+          placeholder="Describe your query"
+          onChange={(e) => setQueryDescription(e.target.value)}
+          />
           <input type="submit" value="Generate query" />
       </form>
     </main>
